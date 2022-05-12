3844d752200e51c8067eadff40d7021b2290c0f8
Caleb Coughenour <CalebCoughenour@protonmail.com>
2022-05-12 11:50:56 -0700

    WIP: continue working out problem connecting pages
    Co-authored-by: JeremyM45 <JeremyM45@users.noreply.github.com>
    Co-authored-by: jfox25 <jfox25@users.noreply.github.com>

--- Organizer/Controllers/ArtistController.cs
+++ Organizer/Controllers/ArtistController.cs
@@ -12,10 +12,31 @@ namespace Organizer.Controllers
       List<Artist> allArtists = Artist.GetArtists();
       return View(allArtists);
     }
+    
+    [HttpGet("/artist/new")]
+    public ActionResult New()
+    {
+      return View();
+    }
+    
+    [HttpGet("/artists/{artistName}")]
+    public ActionResult Show(string artistName)
+    {
+      Artist artist = Artist.FindArtist(artistName);
+      return View(artist);
+    }
     [HttpPost("/artists")]
     public ActionResult Create(string artistName)
     {
       Artist newArtist = new Artist(artistName);
+      return RedirectToAction("Index");
+    }
+    [HttpPost("/artist/{artistName}/records")]
+    public ActionResult New(string albumName, string artistName)
+    {
+      Artist artist = Artist.FindArtist(artistName);
+      Album album = new Album(albumName, artistName);
+      artist.Albums.Add(album);
       return RedirectToAction("Index");
     }
   }
--- Organizer/Controllers/RecordsController.cs
+++ Organizer/Controllers/RecordsController.cs
@@ -13,30 +13,10 @@ namespace Organizer.Controllers
       return View(artists);
     }
     [HttpGet("/artist/{artistName}/record/new")]
-    public ActionResult New()
+    public ActionResult New(string artistName)
     {
-      List<Artist> artists = Artist.GetArtists();
-      return View(artists);
-    }
-    [HttpPost("/records")]
-    public ActionResult New(string albumName, string artist)
-    {
-      Album album = new Album(albumName, artist);
-      List<Artist> artists = Artist.GetArtists();
-      foreach(Artist artist in artists)
-      {
-        if(artist.Name == artist)
-        {
-          artist.Albums.add(album);
-        }
-      }
-      return RedirectToAction("Show");
+      // List<Artist> artists = Artist.GetArtists();
+      return View(artistName);
     }
   }
 }
-    [HttpGet("/categories/{categoryId}/items/new")]
-    public ActionResult New(int categoryId)
-    {
-      Category category = Category.Find(categoryId);
-      return View(category);
-    }
